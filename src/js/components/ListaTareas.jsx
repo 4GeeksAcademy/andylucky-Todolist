bed532a9d6fc643927815c27e5146e8602a508fe
andylucky <3726817+andylucky@users.noreply.github.com>
2025-04-09 10:22:41 +0000

    update refactorizacion

--- src/js/components/ListaTareas.jsx
+++ src/js/components/ListaTareas.jsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Tarea } from "./Tarea";
 
-//create your first component
+//Componente ListaTareas
 export const ListaTareas = (props) => {
 
-	  const [tareas, setTareas] = useState([]);
-       const [inputValue, setInputValue] =useState("");
-      let tareaNueva =  inputValue
+	const [tareas, setTareas] = useState([]);
+    const [inputValue, setInputValue] =useState("");
+
 	
+
+	//funcion que añade nueva tarea
 	const handleKeyDown = (event) => {
 		if(event.key==="Enter"){
-		 setTareas([...tareas, tareaNueva])
-
+		 setTareas([...tareas, inputValue])
 		}
-
 	}
 
+	//funcion para eliminar la tarea donde se hace click en la x
 	const envtoNewTarea = (indiceTarea) => {
-		
 		const nuevasTareas = [...tareas]
 		nuevasTareas.splice(indiceTarea,1)
 	    setTareas(nuevasTareas);
-		
 	}
+
+
+	//funcion que se actualiza cuando cambia  tareas
 	useEffect(() => {
 		console.log("useEffect ejecutado. Tareas actuales:", tareas);
-		document.querySelector(".container > input").value = "";
-
+		setInputValue("")
 	}, [tareas]); 
 
 
 	return (
-		<div class="container d-flex cutomList">
-
-			<input type="text" onChange={e => setInputValue(e.target.value)} onKeyDown={handleKeyDown}   placeholder="Añadir nueva tarea..." />
+		<div className="container d-flex cutomList">
+			<input type="text" onChange={e => setInputValue(e.target.value)} onKeyDown={handleKeyDown} value={inputValue}   placeholder="Añadir nueva tarea..." />
 			<Tarea nuevaTarea={tareas} nuevaTareaOver={envtoNewTarea} />
 		</div>
 	)
--- src/js/components/Tarea.jsx
+++ src/js/components/Tarea.jsx
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
 
 export const Tarea = (props) => {
-	debugger
     let arrayTareas = props.nuevaTarea;
-    const [classTarea, setClassTarea] = useState([]);
-
- 
-
-    
-    // const eventDelete = (event) => {
-    //     debugger
-    //         event.target.className = "task putX";
-    //         const index =arrayTareas.indexOf(event.target.textContent);
-         
-    // }       
-
-
    return(
 	  <>
          <div className="task-list">
                 {arrayTareas.map((tarea, index) => (
-                    <div key={index} className="task" >
-                       
-                    {tarea}
-                    
+                    <div key={index} className="task" >   
+                    {tarea}             
                     <span className="laX" onClick={()=> props.nuevaTareaOver(index)}>x</span>
                     </div>
-
-                    
-                    // onMouseLeave={() => console.log(`Mouse salió de la tarea: ${tarea}`)}
                 ))}
             </div>
     </>
